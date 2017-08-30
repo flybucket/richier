60f2c6f2b3185f88bcf4465d2f73550d18321c7f
Tina Zhao <tinayzhao@gmail.com>
2017-08-30 01:09:52 -0700

    add splash loop video replacement

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const navWrapper = document.getElementById('nav__wrapper')
 const navLogo = document.getElementsByClassName('nav__logo')[0]
+const splashLoop = document.getElementById('splash_loop')
 
 function adjsutNavigationHeight () {
   if (window.scrollY >= 100) {
@@ -17,4 +18,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
 window.addEventListener('scroll', () => {
   adjsutNavigationHeight()
+})
+
+splashLoop.addEventListener('click', () => {
+  console.log('replace with video')
+  const videoPlayer = document.createElement('div')
+  videoPlayer.classList.add('video__player')
+  videoPlayer.innerHTML = '<iframe src="https://player.vimeo.com/video/231239209?title=false&portrait=false&byline=false&autoplay=true" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'
+  splashLoop.parentNode.replaceChild(videoPlayer, splashLoop)
 })
